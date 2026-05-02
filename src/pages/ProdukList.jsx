880b4ea06f109b854cd93bbe4378e4435b261507
BungaDoR <oktaviab14@gmail.com>
2026-05-02 10:15:36 +0700

    first commit

---
+++ src/pages/ProdukList.jsx
@@ -0,0 +1,10 @@
+function ProdukList() {
+    return (
+        <div className="min-h-screen bg-gray-100 p-8">
+            <h1 className="text-4xl font-bold">Produk List</h1>
+            <p className="text-center text-gray-600 mt-4">Halaman Produk List (sedang dibuat)</p>
+        </div>
+    );
+}
+
+export default ProdukList;
